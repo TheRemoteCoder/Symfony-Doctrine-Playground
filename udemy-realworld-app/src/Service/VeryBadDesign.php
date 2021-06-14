6fe7dd80a328207bb1b988301c09eb6900eae742
Christian Oellers <2101569+ChristianOellers@users.noreply.github.com>
2021-06-14 14:42:34 +0200

    .

---
+++ udemy-realworld-app/src/Service/VeryBadDesign.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Service;
+
+use App\Service\Greeting; // Optional use?
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Old pattern <= Symfony 4 to access services from container on fly.
+ * Not recommended anymore as Dependency Injection should be prefered.
+ */
+class VeryBadDesign implements ContainerAwareInterface
+{
+  /*+
+   * @required
+   */
+  public function setContainer(?ContainerInterface $container = null)
+  {
+    $container->get(Greeting::class);
+  }
+}
