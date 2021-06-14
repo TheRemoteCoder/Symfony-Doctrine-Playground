78655c4ddb3064839b05b8076cec5e81a68bc729
Christian Oellers <2101569+ChristianOellers@users.noreply.github.com>
2021-06-14 12:33:41 +0200

    .

--- udemy-realworld-app/src/Security/ExampleVoter.php
+++ udemy-realworld-app/src/Security/ExampleVoter.php
@@ -2,8 +2,18 @@
 
 namespace App\Security;
 
-class ExampleVoter
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\VoterInterface;
+
+class ExampleVoter implements VoterInterface
 {
-
-
+    /**
+     * Return vote for given parameters.
+     *
+     * @see VoterInterface
+     */
+    public function vote(TokenInterface $token, $subject, array $attributes) : int
+    {
+      return self::ACCESS_GRANTED;
+    }
 }
