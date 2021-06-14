d473f54de19c5f91fe1f49574387bacb4a1a35b3
Christian Oellers <2101569+ChristianOellers@users.noreply.github.com>
2021-06-14 17:19:14 +0200

    .

--- udemy-realworld-app/src/Service/Greeting.php
+++ udemy-realworld-app/src/Service/Greeting.php
@@ -6,15 +6,20 @@ use Psr\Log\LoggerInterface;
 
 class Greeting
 {
-    public function __construct(LoggerInterface $logger)
-    {
-        $this->logger = $logger;
-    }
+  /**
+   * Symfony 'issue': Autowiring a string cannot be done as it is not a service, just a type;
+   * so it needs to be configured as argument.
+   */
+  public function __construct(LoggerInterface $logger, string $message)
+  {
+    $this->logger = $logger;
+    $this->message = $message;
+  }
 
-    public function greet(string $name): string
-    {
-        $this->logger->info("Greeted: $name");
+  public function greet(string $name): string
+  {
+    $this->logger->info("Greeted: $name");
 
-        return "Hello $name";
-    }
+    return "{$this->message} $name";
+  }
 }
