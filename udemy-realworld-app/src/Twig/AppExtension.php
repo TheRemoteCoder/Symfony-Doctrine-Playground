62e73f862255dcbddc26185933869a99b7bb790c
Christian Oellers <2101569+ChristianOellers@users.noreply.github.com>
2021-06-17 14:01:26 +0200

    .

--- udemy-realworld-app/src/Twig/AppExtension.php
+++ udemy-realworld-app/src/Twig/AppExtension.php
@@ -7,6 +7,11 @@ use Twig\TwigFilter;
  * Automatically picked up as a service, because we extend a certain class that is part of
  * what can get mapped to services (Service container looks for classes that extend 'AbstractExtension'; among others).
  * Here is no need to map it in any config YAML. Works because we use the system default YAML files.
+ *
+ * @example
+ *   php bin/console debug:container 'App\Twig\AppExtension'
+ *
+ * @todo Decide - Should this be a subfolder to Service/ as it basically is one?
  */
 class AppExtension extends AbstractExtension
 {
