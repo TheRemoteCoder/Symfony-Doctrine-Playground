9cfd205db48239d0351fb75f6ef9973b6d2c9791
Christian Oellers <2101569+ChristianOellers@users.noreply.github.com>
2021-06-21 12:00:44 +0200

    .

--- udemy-realworld-app/src/Twig/AppExtension.php
+++ udemy-realworld-app/src/Twig/AppExtension.php
@@ -3,6 +3,7 @@
 namespace App\Twig;
 
 use Twig\Extension\AbstractExtension;
+use Twig\Extension\GlobalsInterface;
 use Twig\TwigFilter;
 
 /**
@@ -15,11 +16,23 @@ use Twig\TwigFilter;
  *
  * @todo Decide - Should this be a subfolder to Service/ as it basically is one?
  */
-class AppExtension extends AbstractExtension
+class AppExtension extends AbstractExtension implements GlobalsInterface
 {
+  public function __construct(string $locale)
+  {
+    $this->locale = $locale;
+  }
+
   public function getFilters()
   {
     return [new TwigFilter('price', [$this, 'priceFilter'])];
+  }
+
+  public function getGlobals(): array
+  {
+    return [
+      'locale' => $this->locale,
+    ];
   }
 
   public function priceFilter(float $price)
