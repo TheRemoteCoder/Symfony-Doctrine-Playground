6fcbe24c8c472c80aef6c0f82feff9c5c4e9fae3
Christian Oellers <2101569+ChristianOellers@users.noreply.github.com>
2021-10-25 12:39:00 +0200

    .

---
+++ udemy-realworld-app/src/Form/MicroPostType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\MicroPost;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextAreaType;
+use Symfony\Component\OptionsResolver\Exception\AccessException;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MicroPostType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('text', TextAreaType::class, ['label' => false])
+            ->add('save', SubmitType::class);
+    }
+
+    /**
+     * Set data operations against entity, so any form data entered
+     * will result in class instances (?).
+     *
+     * @param OptionsResolver $resolver
+     * @return void
+     * @throws AccessException
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => MicroPost::class,
+        ]);
+    }
+}
